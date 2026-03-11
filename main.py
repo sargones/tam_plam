2e276eac81d5486dcfaf44255807a70e5166889f
sargones <pipi.sargones@gmail.com>
2026-03-11 19:40:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+#from main import artist_df
+
+st.set_page_config(page_title="Tam & Plam 19 July 2026", page_icon="👋")
+
+st.title("Tam & Plam 19 July 2026")
+st.subheader("Generic information")
