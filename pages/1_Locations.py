dae06da4f2c7b2179c30989f68704830a3e6ccb5
sargones <pipisargones@gmail.com>
2026-03-11 19:57:39 +0100

    first changes

--- pages/1_Locations.py
+++ pages/1_Locations.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 # from main import artist_df
 
-st.set_page_config(page_title="Tam & Plam 19 July 2026", page_icon="👋")
+st.set_page_config(page_title="Locations info", page_icon="👋")
 
 st.title("Tam & Plam 19 July 2026")
 st.subheader("Generic information")
