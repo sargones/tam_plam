7afbaacf78d2ff36ec14b5ea4d58421c1352f166
sargones <pipisargones@gmail.com>
2026-03-13 19:15:04 +0100

    s

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.markdown("""
     <style>
         div[data-testid="stVerticalBlock"] > div:has(div.stMarkdown) {
             position: sticky;
-            top: 0;
+            top: 10;
             z-index: 999;
             background-color: white;
         }
